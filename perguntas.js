87ef1e10b0f96c24dd00533b55766f09987d0f17
jorgeponto <00001102205588sp@al.educacao.sp.gov.br>
2024-11-19 15:27:30 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'POKÉMON',
+    'Qual pokémon que representa a morte?',
+    'Yvital'
 )
 
 criaCartao(
